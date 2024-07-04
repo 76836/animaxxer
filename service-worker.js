1d84883e0d8f6e4982702dbcb88f76e693dbff22
76836 <74463275+76836@users.noreply.github.com>
2024-07-04 15:22:04 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,44 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('my-pwa-cache').then((cache) => {
-      return cache.addAll([
+const CACHE_NAME = 'cache-v2';
+const urlsToCache = [
         './',
         './index.html',
         './background.png',
-        './manifest.json'
-      ]);
+        './manifest.json',
+        './sound.mp3'
+];
+
+// Install event - caching files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate event - cleaning up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
 
+// Fetch event - serving cached content
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
